9f91cd91216ac8ee91fc91e838d145f22370f0df
fafawds67685da <gdev6886@gmail.com>
2024-12-06 09:16:03 +0530

    Finally !

---
+++ src/assets/Data.js
@@ -0,0 +1,26 @@
+export const data=[
+    {
+    Question: "What among these is a non-linear kind of Data Structure ?",
+    Option1:"Linked List",
+    Option2:"Array",
+    Option3:"Tree",
+    Option4:"Stack",
+    ans:3,
+    },
+    {
+        Question: "What among these is a linear kind of Data Structure ?",
+        Option1:"Graph",
+        Option2:"Array",
+        Option3:"Tree",
+        Option4:"Binary tree",
+        ans:2,
+        },
+        {
+            Question: "DEv BHAI",
+            Option1:"Normal",
+            Option2:"Crazy",
+            Option3:"GOD",
+            Option4:"DEddy",
+            ans:3,
+            }
+]
